f237fcdb18fa9808d9c52326391954c220bd0a9b
virusstock <firusstock@mail.ru>
2022-08-05 13:44:54 +0300

    Huge changes

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const api_key = 'PUwBTbX3OoBfsQRuyaQstmkJUMwF7zthjIUC76qB';
 
-async function changeImage() {
+async function changeImage(date) {
 
-    const request = `https://api.nasa.gov/planetary/apod?api_key=${api_key}`;
-    let response = await fetch(request);
-    let imageData = await response.json();
+    const request = `https://api.nasa.gov/planetary/apod?api_key=${api_key}&date=${date}`;
+
+    const response = await fetch(request);
+    const imageData = await response.json();
     console.log(imageData);
 
-    let image = document.querySelector('img');
+    const image = document.querySelector('#picture-of-the-day');
     image.src = imageData.url;
+
+    const explanation = document.querySelector('#explanation');
+    explanation.innerHTML = imageData.explanation;
+
+    return false;
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('.calendar');
+    form.onsubmit = function() {
+        changeImage(this.querySelector('#calendar1').value);
+        
+        return false;
+    }
+
+    changeImage('2022-08-03');
+});
