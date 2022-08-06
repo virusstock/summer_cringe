0965f1ad3d72e382735224651904150348dc2be1
LohLohovsky <lohlohovsky1@mail.ru>
2022-08-06 14:19:37 +0300

    Change function 'createCalendar'
    Signature changed to 'createCalendar(month, year)'.
    Improved layout:
    - first day of month is set to proper day of week;
    - added placeholders for days of previous and next months.

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,12 +1,43 @@
 
-document.addEventListener('DOMContentLoaded', function() {
+function createCalendar(month, year) {
+    // Date(year, monthIndex, day)
+    // monthIndex starts from 0 for January
+    // (day < 1) means day of previous month
+    // (day > days in month) means day of next month
+    const date = new Date(year, month - 1, 1);
+    const firstDay = date.getDay();
+    const numberOfDays = new Date(year, month, 0).getDate();
+
+
+
+
+
+
 
     const calendar = document.querySelector('.custom-calendar');
-
-    for (let i = 1; i <= 30; i++) {
+    calendar.innerHTML = '';
+    let daysCount = 0;
+    for (let i = 0; i < (firstDay + 7 - 1) % 7; i++) {
+        const item = document.createElement('div');
+        item.className = 'calendar-item not-selected-calendar-item';
+        item.innerHTML = '';
+        calendar.appendChild(item);
+        daysCount++;
+    }
+    for (let i = 1; i <= numberOfDays; i++) {
         const item = document.createElement('div');
         item.className = 'calendar-item not-selected-calendar-item';
         item.innerHTML = i;
         calendar.appendChild(item);
+        daysCount++;
     }
-});
+    for (let i = 0; i < (7 - (daysCount % 7)) % 7; i++) {
+        const item = document.createElement('div');
+        item.className = 'calendar-item not-selected-calendar-item';
+        item.innerHTML = '';
+        calendar.appendChild(item);
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', () => createCalendar(8, 2022));
