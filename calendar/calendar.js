447191d58bf1ff92d098fe2230d6c092acc7f9c5
LohLohovsky <lohlohovsky1@mail.ru>
2022-08-16 13:32:24 +0300

    Refactor
    - rename some identifiers
    - split header to control and monthInfo
    - add CalendarPage class for more convenient monthInfo and body creation

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,114 +1,180 @@
-function changeToPreviousMonth() {
-    const calendar = document.querySelector('.custom-calendar');
-    const date = new Date(document.forms['date-form'].elements['date'].value);
-    date.setDate(0);
-    calendar.innerHTML = '';
-    calendar.appendChild(createCalendarHeader(date));
-    calendar.appendChild(createCalendarBody(date));
-    const dateForm = document.forms['date-form'];
-    const dateInput = dateForm.elements['date'];
-    dateInput.value = date.toISOString().substring(0, 10);
+class CalendarPage {
+
+    // Date(year, monthIndex, day)
+    // (monthIndex == 0) means January
+    // Date.getDay() returns day of week (0 is Sunday)
+    // (day < 1) means day of previous month
+    // (day > days in month) means day of next month
+    // (month < 1) means month of previous year
+    // (month > months in year) means month of next year
+
+    constructor(date) { 
+        this.date = date ? new Date(date) : new Date();
+    }
+
+    get nextMonthDate() {
+        return new Date(this.date.getFullYear(), this.date.getMonth() + 1);
+    }
+
+    get prevMonthDate() {
+        return new Date(this.date.getFullYear(), this.date.getMonth() - 1);
+    }
+
+    get firstDayDate() {
+        return new Date(this.date.getFullYear(), this.date.getMonth(), 1);
+    }
+
+    get lastDayDate() {
+        return new Date(this.date.getFullYear(), this.date.getMonth() + 1, 0);
+    }
+
+    toNext() {
+        this.date = this.nextMonthDate;
+    }
+
+    toPrev() {
+        this.date = this.prevMonthDate;
+    }
+
+    get next() {
+        return new CalendarPage(this.nextMonthDate);
+    }
+
+    get prev() {
+        return new CalendarPage(this.prevMonthDate);
+    }
+
+    get monthName() {
+        return this.date.toLocaleString('en-US', {month: 'long'});
+    }
+
+    get monthIndex() {
+        return this.date.getMonth();
+    }
+
+    get year() {
+        return this.date.getFullYear();
+    }
+
+    get firstDay() {
+        return this.firstDayDate.getDay();
+    }
+
+    get lastDay() {
+        return this.lastDayDate.getDay();
+    }
+
+    get days() {
+        return this.lastDayDate.getDate();
+    }
+
+    get weeks() {
+        return Math.ceil((this.prev.lastDay + this.days) / 7);
+    }
 }
 
 
-function changeToNextMonth() {
-    const calendar = document.querySelector('.custom-calendar');
-    const date = new Date(document.forms['date-form'].elements['date'].value);
-    date.setDate(32);
-    calendar.innerHTML = '';
-    calendar.appendChild(createCalendarHeader(date));
-    calendar.appendChild(createCalendarBody(date));
-    const dateForm = document.forms['date-form'];
-    const dateInput = dateForm.elements['date'];
-    dateInput.value = date.toISOString().substring(0, 10);
-}
-
-
-function createCalendarHeader(date) {
-    const sDate = date.toLocaleString('en-US', {month: 'long', year: 'numeric'});
-    const [sMonth, sYear] = sDate.split(' ');
+function createCalendarControl(downArrowOnClick, upArrowOnClick) {
 
     const downArrow = document.createElement('span');
     downArrow.innerHTML = '↓';
     downArrow.className = 'calendar-arrow';
-    downArrow.onclick = changeToNextMonth;
+    downArrow.onclick = downArrowOnClick;
+
     const upArrow = document.createElement('span');
     upArrow.innerHTML = '↑';
     upArrow.className = 'calendar-arrow';
-    upArrow.onclick = changeToPreviousMonth;
-    const arrows = document.createElement('div');
-    arrows.className = 'calendar-header-arrows';
-    arrows.appendChild(downArrow);
-    arrows.appendChild(upArrow);
+    upArrow.onclick = upArrowOnClick;
 
-    const dateInfo = document.createElement('div');
-    dateInfo.className = 'calendar-header-date-info';
-    dateInfo.innerHTML = `${sMonth} ${sYear}`;
+    const control = document.createElement('div');
+    control.className = 'calendar-control';
+    control.appendChild(downArrow);
+    control.appendChild(upArrow);
 
-    const calendarHeader = document.createElement('div');
-    calendarHeader.className = 'calendar-header';
-    calendarHeader.appendChild(arrows);
-    calendarHeader.appendChild(dateInfo);
-
-    return calendarHeader;
+    return control;
 }
 
 
-function createCalendarBody(date) {
-    // Date.getDay() returns day of week (0 is Sunday)
-    // (day < 1) means day of previous month
-    // (day > days in month) means day of next month
-    const firstDay = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
-    const numberOfDays = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
-    const prevMonthNumberOfDays = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
-    const prevMonthDaysToShow = (firstDay + 6) % 7;
-    const prevMonthFirstDayToShow = prevMonthNumberOfDays - prevMonthDaysToShow + 1;
-    const nextMonthDaysToShow = (7 - (numberOfDays + prevMonthDaysToShow) % 7) % 7;
+function createCalendarMonthInfo(calendarPage) {
 
-    const calendarBody = document.createElement('div');
-    calendarBody.className = 'calendar-body';
+    const monthInfo = document.createElement('div');
+    monthInfo.className = 'calendar-month-info';
+    monthInfo.innerHTML = `${calendarPage.monthName} ${calendarPage.year}`;
 
-    function addCalendarItems(firstDay, lastDay, className) {
+    return monthInfo;
+}
+
+
+function createCalendarBody(calendarPage) {
+
+    const prevPage = calendarPage.prev;
+
+    const days = calendarPage.days;
+    const prevMonthDays = prevPage.days;
+    const prevMonthDaysToShow = prevPage.lastDay;
+    const prevMonthFirstDayToShow = prevMonthDays - prevMonthDaysToShow + 1;
+    const nextMonthDaysToShow = (7 - calendarPage.lastDay) % 7;
+
+    const body = document.createElement('div');
+    body.className = 'calendar-body';
+
+    function addItems(firstDay, lastDay, className) {
         for (let day = firstDay; day <= lastDay; day++) {
             const item = document.createElement('div');
             item.className = `calendar-item ${className}`;
             item.innerHTML = day;
-            calendarBody.appendChild(item);
+            body.appendChild(item);
         }
     }
 
-    addCalendarItems(prevMonthFirstDayToShow, prevMonthNumberOfDays, 'not-selected-other-calendar-item');
-    addCalendarItems(1, numberOfDays, 'not-selected-calendar-item');
-    addCalendarItems(1, nextMonthDaysToShow, 'not-selected-other-calendar-item');
+    addItems(prevMonthFirstDayToShow, prevMonthDays, 'not-selected-other-calendar-item');
+    addItems(1, days, 'not-selected-calendar-item');
+    addItems(1, nextMonthDaysToShow, 'not-selected-other-calendar-item');
     
-    return calendarBody;
+    return body;
+}
+
+
+function updateCalendar(calendar, calendarPage) {
+    
+    const monthInfo = calendar.querySelector('.calendar-month-info');
+    const body = calendar.querySelector('.calendar-body');
+
+    const newMonthInfo = createCalendarMonthInfo(calendarPage);
+    const newBody = createCalendarBody(calendarPage);
+
+    monthInfo.replaceWith(newMonthInfo);
+    body.replaceWith(newBody);
 }
 
 
 function createCalendar(date) {
-    // Date(year, monthIndex, day)
-    // (monthIndex == 0) means January
-    const calendarHeader = createCalendarHeader(date);
-    const calendarBody = createCalendarBody(date);
+
     const calendar = document.createElement('div');
-    calendar.innerHTML = '';
-    calendar.appendChild(calendarHeader);
-    calendar.appendChild(calendarBody);
+    calendar.className = 'custom-calendar';
+
+    const calendarPage = new CalendarPage(date);
+    
+    const monthInfo = createCalendarMonthInfo(calendarPage);
+    const body = createCalendarBody(calendarPage);
+    const control = createCalendarControl(
+        () => {
+            calendarPage.toNext();
+            updateCalendar(calendar, calendarPage);
+        },
+        () => {
+            calendarPage.toPrev();
+            updateCalendar(calendar, calendarPage);
+        }
+    );
+
+    const header = document.createElement('div');
+    header.className = 'calendar-header';
+    header.appendChild(control);
+    header.appendChild(monthInfo);
+
+    calendar.appendChild(header);
+    calendar.appendChild(body);
+    
     return calendar;
 }
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    const date = new Date();
-    const dateForm = document.forms['date-form'];
-    const dateInput = dateForm.elements['date'];
-    dateInput.style = 'display: none;';
-    dateInput.value = date.toISOString().substring(0, 10);
-    dateForm.onsubmit = function() {
-        console.log(dateInput.value);
-        return false;
-    }
-    const calendar = createCalendar(date);
-    calendar.className = 'custom-calendar';
-    dateInput.before(calendar);
-});
